87b7c12835e0fb965950a0362ac40a27395135ba
Priyadarshini <priyadarshini.raja@ideas2it.com>
2022-04-25 13:34:56 +0530

    Completed MVC

---
+++ TraineesManagementSystem/TraineesManagement.service/ITraineeService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TraineesManagement.model;
+
+namespace TraineesManagement.service
+{
+    public interface ITraineeService
+    {
+        List<Trainee> GetAllTrainees();
+        int DeleteTrainee(int id);
+        int AddTrainee(Trainee trainee);
+        Trainee GetTraineeById(int id);
+        int UpdateTrainee(Trainee trainee);
+    }
+}
---
+++ TraineesManagementSystem/TraineesManagement.service/TraineeService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraineesManagement.data;
+using TraineesManagement.model;
+
+namespace TraineesManagement.service
+{
+    public class TraineeService : ITraineeService
+    {
+        ITraineesDBManager traineesDBManager = new TraineesDBManager();
+
+        public int AddTrainee(Trainee trainee)
+        {
+            return traineesDBManager.InsertTrainee(trainee);
+        }
+        public List<Trainee> GetAllTrainees()
+        {
+            return traineesDBManager.GetAllTrainees().ToList();
+        }
+
+        public int DeleteTrainee(int id)
+        {
+            return traineesDBManager.DeleteTrainee(id);
+        }
+
+        public Trainee GetTraineeById(int id)
+        {
+            return traineesDBManager.GetTraineeById(id);
+        }
+
+        public int UpdateTrainee(Trainee trainee)
+        {
+            return traineesDBManager.UpdateTrainee(trainee);
+        }
+    }
+}
